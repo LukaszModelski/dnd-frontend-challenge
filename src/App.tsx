288513eedf3483cfbd997a0fef75cd5087e60578
Czapa <lukaszmodelski90@gmail.com>
2023-09-13 21:12:04 +0200

    Initial webpack config

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const App = () => <h1>DnD Beyond frontend challenge</h1>
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, '/')
+    }
+  }
+}
